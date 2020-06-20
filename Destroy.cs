a37457480b1d229286a0b461493e9f86701c7d88
Muslim Cheberaheng <66585845+MuslimCheberaheng@users.noreply.github.com>
2020-06-20 19:58:07 +0700

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -10,11 +10,9 @@ public class Destroy : MonoBehaviour
         
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+    {        
         Plat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-15f, 15f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))), Quaternion.identity);
-        Destroy(collision.gameObject);
-
-        
+        Destroy(collision.gameObject);        
+        // Instantiate the platform then destroy after the player passed it 
     }
 }
